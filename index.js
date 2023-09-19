36c0b18d3e3a72421fc165d414b2c3c4924f3c50
+ <+>
2023-09-20 01:08:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    return callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      console.log('This is a named function');
+    };
+  }
+  function returnsAnAnonymousFunction() {
+    return  () => {
+        console.log('This is an anonymous function'); 
+    };
+  }
